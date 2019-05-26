1e60c3fd3ce6c8bd07dd7ba15993ac9fb0acc35a
67owilliams <37914797+67owilliams@users.noreply.github.com>
2019-05-26 12:12:25 -0500

    Delete BasicString.py

--- BasicString.py
+++
@@ -1,5 +0,0 @@
-print ('Hello There (Single Quote) !')
-print ("Hello There (Double Quote) !")
-print ("""This is a multiple line
-string using triple double quotes.
-You can also use triple single quotes.""")
